ffaf83bfe37ef32953e5473897299aa84e7d18ce
a18lukto <49182858+a18lukto@users.noreply.github.com>
2021-04-20 16:09:25 +0200

    Created a script to test the application
    New script for testing

---
+++ MessureTools/script.js
@@ -0,0 +1,58 @@
+//-----------------------------------------------
+//----------------Time saver---------------------
+var startTime = new Date().getTime();
+var counter = parseInt(localStorage.getItem("TimerCounter", 0));
+
+function startUp() {
+     /* Om det är första gången det startas sätts start värdet till 0 och med,
+     ett ',' efter för tydlighet. */
+    if (localStorage.getItem("Loaded page") == null) {
+        localStorage.setItem("Loaded page", 0 + " \n");
+    }
+    // Om countern är null sätts den till 0. 
+    if (localStorage.getItem("TimerCounter") == null) {
+        localStorage.setItem("TimerCounter", 0) + " \n";
+    }
+    // Ser till att den itereras endast 5 gånger.
+    for (i =0; i < 2; i++)   {
+        //delta = 0;
+        var endTime = new Date().getTime();
+        var theTime = endTime - startTime;
+        endTime = localStorage.setItem("Time", JSON.stringify(theTime)); 
+        counter++;
+        buttonClick();
+        localStorage.getItem(startTime);
+        var delta = localStorage.getItem("Loaded page");
+        var measureDiff = theTime;
+        console.log(theTime)
+        delta += measureDiff - 2000 + "\n";
+
+        localStorage.setItem("Loaded page", delta);
+    }
+}
+//-----------------------------------------------
+//----------------Clicker------------------------
+
+ function buttonClick() {
+    selectedYear = 1968
+    //Klickar på knappen med id logo
+    var clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });            
+                                //  
+                document.getElementById("Dataset")[0].dispatchEvent(clickEvent) 
+                document.getElementById("hamtaData").dispatchEvent(clickEvent)
+                for(var x = 0;x < 54; x++){ 
+                    let donePageLoading = document.readyState;
+                    document.getElementById("LandYear")[x].dispatchEvent(clickEvent)
+                        console.log(document.getElementById("LandYear")[x])             // Click hamtaData button
+                        document.getElementById("renderMap").dispatchEvent(clickEvent)
+                        selectedYear++
+                        console.log(selectedYear)
+                        console.log(document.getElementById("renderMap").dispatchEvent(clickEvent))
+                        console.log("sucessfull timeout")
+                        
+                      }
+}

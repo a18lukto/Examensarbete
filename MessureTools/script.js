dc042d04262dea7044bd8a4f4851c726c0b05e9b
a18lukto <49182858+a18lukto@users.noreply.github.com>
2021-04-22 09:29:41 +0200

    Fixed d3 and created a testing script
    Script to test loading time for heatmap
    Fixed D3 html file to work better and work with the testing script

--- MessureTools/script.js
+++ MessureTools/script.js
@@ -2,7 +2,6 @@
 //----------------Time saver---------------------
 var startTime = new Date().getTime();
 var counter = parseInt(localStorage.getItem("TimerCounter", 0));
-
 function startUp() {
      /* Om det är första gången det startas sätts start värdet till 0 och med,
      ett ',' efter för tydlighet. */
@@ -14,7 +13,7 @@ function startUp() {
         localStorage.setItem("TimerCounter", 0) + " \n";
     }
     // Ser till att den itereras endast 5 gånger.
-    for (i =0; i < 2; i++)   {
+    for (i =0; i < 500; i++)   {
         //delta = 0;
         var endTime = new Date().getTime();
         var theTime = endTime - startTime;
@@ -25,7 +24,7 @@ function startUp() {
         var delta = localStorage.getItem("Loaded page");
         var measureDiff = theTime;
         console.log(theTime)
-        delta += measureDiff - 2000 + "\n";
+        delta += measureDiff + "\n";
 
         localStorage.setItem("Loaded page", delta);
     }
@@ -35,6 +34,7 @@ function startUp() {
 
  function buttonClick() {
     selectedYear = 1968
+    //selectedDataset = "folktathet"
     //Klickar på knappen med id logo
     var clickEvent = new MouseEvent('click', {
         view: window,
@@ -42,9 +42,9 @@ function startUp() {
         cancelable: true
     });            
                                 //  
-                document.getElementById("Dataset")[0].dispatchEvent(clickEvent) 
+                document.getElementById("Dataset")[1].dispatchEvent(clickEvent) 
                 document.getElementById("hamtaData").dispatchEvent(clickEvent)
-                for(var x = 0;x < 54; x++){ 
+                for(var x = 0;x < 52; x++){ 
                     let donePageLoading = document.readyState;
                     document.getElementById("LandYear")[x].dispatchEvent(clickEvent)
                         console.log(document.getElementById("LandYear")[x])             // Click hamtaData button
